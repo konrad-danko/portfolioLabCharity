f01aed0cc94dcce9b951428fd2f9fa3c57b4cce4
Konrad Dańko <konrad.danko@gmail.com>
2021-02-11 23:53:44 +0100

    Commit of project Category and CategoryRepository

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,42 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(max = 50, message = "Dopuszczalna ilość znaków wynosi 50")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/pl/coderslab/charity/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package pl.coderslab.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pl.coderslab.charity.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
