48569e96ad3c0cd234a2982ed8892ebad9cee2f7
Konrad Dańko <konrad.danko@gmail.com>
2021-02-19 00:40:11 +0100

    Commit of creation of User and UserRepository files

---
+++ src/main/java/pl/coderslab/charity/model/User.java
@@ -0,0 +1,92 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(max = 100, message = "Dopuszczalna ilość znaków wynosi 100")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Pole nie może być puste")
+    @Size(max = 100, message = "Dopuszczalna ilość znaków wynosi 100")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Pole nie może być puste")
+    @Email(message = "Wpisz poprawny adres email")
+    @Size(max = 50, message = "Dopuszczalna ilość znaków wynosi 50")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Pole nie może być puste")
+    private String password;
+
+    @ManyToOne
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
