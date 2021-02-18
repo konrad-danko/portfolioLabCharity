b396e400e7ecafd8d87b304a1f89313552a104b3
Konrad Dańko <konrad.danko@gmail.com>
2021-02-19 00:41:15 +0100

    Commit of creation of Role and RoleRepository files

---
+++ src/main/java/pl/coderslab/charity/model/Role.java
@@ -0,0 +1,37 @@
+package pl.coderslab.charity.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String roleName;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
