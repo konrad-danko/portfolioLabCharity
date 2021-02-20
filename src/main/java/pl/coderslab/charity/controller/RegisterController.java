8e35c74249a79667a9592c228d6778bbc1304373
Konrad Dańko <konrad.danko@gmail.com>
2021-02-21 00:05:43 +0100

    Commit of update of RegisterController.java

--- src/main/java/pl/coderslab/charity/controller/RegisterController.java
+++ src/main/java/pl/coderslab/charity/controller/RegisterController.java
@@ -2,10 +2,16 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.model.User;
 import pl.coderslab.charity.repository.RoleRepository;
+import pl.coderslab.charity.repository.UserRepository;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -13,8 +19,10 @@ import pl.coderslab.charity.repository.RoleRepository;
 public class RegisterController {
 
     private final RoleRepository roleRepository;
-    public RegisterController(RoleRepository roleRepository) {
+    private final UserRepository userRepository;
+    public RegisterController(RoleRepository roleRepository, UserRepository userRepository) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
     }
 
     //create a user
@@ -24,6 +32,15 @@ public class RegisterController {
         user.setRole(roleRepository.findByRoleName("ROLE_USER"));
         model.addAttribute("user", user);
         return "register";
+    }
+
+    @PostMapping(path = "/create")
+    public String processCreateUser(@ModelAttribute @Valid User user, BindingResult result){
+        if(result.hasErrors()){
+            return "register";
+        }
+        userRepository.save(user);
+        return "redirect:/";
     }
 
 
