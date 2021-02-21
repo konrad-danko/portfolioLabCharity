52d969f29424dc0beea3a9b7ab0b8c0c9ec2fac8
Konrad Dańko <konrad.danko@gmail.com>
2021-02-22 00:20:27 +0100

    Creation of UserServiceImpl.java

---
+++ src/main/java/pl/coderslab/charity/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.model.Role;
+import pl.coderslab.charity.model.User;
+import pl.coderslab.charity.repository.RoleRepository;
+import pl.coderslab.charity.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public User findUser(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByRoleName("ROLE_USER");
+        user.setRole(userRole);
+        userRepository.save(user);
+    }
+}
