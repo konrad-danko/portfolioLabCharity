f72e57b00679a165730ab532541befab83c0c45c
Konrad Dańko <konrad.danko@gmail.com>
2021-02-23 08:54:52 +0100

    Update of DonationRepository.java (added 'findAllByInstitution_Id')

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -3,6 +3,10 @@ package pl.coderslab.charity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.Donation;
+import pl.coderslab.charity.model.Institution;
+
+import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
+    List<Donation> findAllByInstitution_Id(long id);
 }
