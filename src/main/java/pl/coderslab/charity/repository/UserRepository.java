ebe25101dccffd284f7f9c1f3ffd903775d42547
Konrad Dańko <konrad.danko@gmail.com>
2021-02-24 00:07:08 +0100

    Update of UserRepository.java (added findAllUsersByRoleIdOrderedByLastName)

--- src/main/java/pl/coderslab/charity/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.model.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    String sqlQueryFindAllUsersByRoleIdOrderedByLastName = "select * from users\n" +
+            "where role_id=?1\n" +
+            "order by last_name";
+    @Query(value = sqlQueryFindAllUsersByRoleIdOrderedByLastName, nativeQuery = true)
+    List<User> findAllUsersByRoleIdOrderedByLastName(int roleId);
 }
