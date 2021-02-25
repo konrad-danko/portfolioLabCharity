b76dd1d55b25da2c3718dc421bdf2abeafee1e9d
Konrad Dańko <konrad.danko@gmail.com>
2021-02-25 20:39:02 +0100

    Update of DonationRepository.java (added 'findAllByUser_Id')

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     List<Donation> findAllByInstitution_Id(long id);
+
+    List<Donation> findAllByUser_Id(long id);
 }
