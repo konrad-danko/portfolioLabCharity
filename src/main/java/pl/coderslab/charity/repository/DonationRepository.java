40bff219fa83de67da302000447976ddaeaac1c4
Konrad Dańko <konrad.danko@gmail.com>
2021-02-28 14:29:16 +0100

    Update of DonationRepository.java (added getUserDonationsSorted(long id))

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -5,10 +5,20 @@ import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.model.Institution;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
     List<Donation> findAllByInstitution_Id(long id);
 
     List<Donation> findAllByUser_Id(long id);
+
+    String sqlQueryGetUserDonationsSorted = "select * from donations\n" +
+            "where user_id=6\n" +
+            "order by\n" +
+            "donation_status_id,\n" +
+            "pick_up_date desc,\n" +
+            "created desc";
+    @Query(value = sqlQueryGetUserDonationsSorted, nativeQuery = true)
+    List<Donation> getUserDonationsSorted(long id);
 }
