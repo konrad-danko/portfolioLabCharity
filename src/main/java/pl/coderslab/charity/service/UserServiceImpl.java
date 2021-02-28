d810c63af361d20cd103f461a1f09f90803ddaec
Konrad Dańko <konrad.danko@gmail.com>
2021-02-28 22:56:08 +0100

    Update of UserServiceImpl.java (replaced user.setEnabled with user.setUserStatus)

--- src/main/java/pl/coderslab/charity/service/UserServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/UserServiceImpl.java
@@ -4,20 +4,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.coderslab.charity.model.Role;
 import pl.coderslab.charity.model.User;
+import pl.coderslab.charity.model.UserStatus;
 import pl.coderslab.charity.repository.RoleRepository;
 import pl.coderslab.charity.repository.UserRepository;
+import pl.coderslab.charity.repository.UserStatusRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final UserStatusRepository userStatusRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     public UserServiceImpl(UserRepository userRepository,
                            RoleRepository roleRepository,
+                           UserStatusRepository userStatusRepository,
                            BCryptPasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.userStatusRepository = userStatusRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,7 +33,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setEnabled(1);
+        UserStatus userStatus = userStatusRepository.findById(1);
+        user.setUserStatus(userStatus);
         Role donatorRole = roleRepository.findById(2);
         user.setRole(donatorRole);
         userRepository.save(user);
